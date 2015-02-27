5ce4de44200cd68f4af23a57367054f4032cd0a4
po0ya <pooya.mosaddegh72@gmail.com>
2015-02-27 19:31:24 +0000

    mail

--- mail_handler.php
+++ mail_handler.php
@@ -1,13 +1,12 @@
 <?php
 if(isset($_POST['submit'])){
-    $to = "email@example.com"; // this is your Email address
+    $to = "pooya.mosaddegh72@gmail.com"; // this is your Email address
     $from = $_POST['email']; // this is the sender's Email address
-    $first_name = $_POST['first_name'];
-    $last_name = $_POST['last_name'];
-    $subject = "Form submission";
+    $name = $_POST['name'];
+    $subject = $_POST['subject'];
     $subject2 = "Copy of your form submission";
-    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
-    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+    $message = $name . " " . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['message'];
 
     $headers = "From:" . $from;
     $headers2 = "From:" . $to;
